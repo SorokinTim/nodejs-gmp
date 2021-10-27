ac02580ea660e7229434e90570d2ba65238cbd50
Tsimafei_Sarokin <Tsimafei_Sarokin@epam.com>
2021-10-27 19:28:17 +0300

    fix: fix password validation pattern

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -9,5 +9,5 @@ export interface UserRequestSchema extends ValidatedRequestSchema {
 export const UserSchema = Joi.object({
     login: Joi.string().required(),
     age: Joi.number().min(4).max(130).required(),
-    password: Joi.string().alphanum().required(),
+    password: Joi.string().pattern(/([a-z][0-9]|[0-9][a-z])+/i).required(),
 });
